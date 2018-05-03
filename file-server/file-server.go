cde412e4adc1e86a1d8509d1ec7dc269f37a8b34
humstarman <lueyoung7@gmail.com>
2018-05-03 15:40:46 +0800

    update: replace Handle with HandleFunc, and in the func introducing the log to record requests of files

--- file-server/file-server.go
+++ file-server/file-server.go
@@ -4,15 +4,22 @@ import (
     "fmt"
     "net/http"
     "flag"
+    "log"
 )
 
 var portFlag = flag.Int("p",2121,"working port")
-var dirFlag = flag.String("d","/mnt","working directory")
+
+var staticfs = http.FileServer(http.Dir("/mnt"))
 
 func main() {
     flag.Parse()
-    dir := *dirFlag + "/"
     port := fmt.Sprintf(":%d",*portFlag)
-    http.Handle("/", http.FileServer(http.Dir(dir)))
+    //http.Handle("/", http.FileServer(http.Dir(dir)))
+    http.HandleFunc("/", static)
     http.ListenAndServe(port, nil)
+}
+
+func static(w http.ResponseWriter, r *http.Request) {
+    log.Printf("Access static file: %s\n", r.URL.Path)
+    staticfs.ServeHTTP(w, r)
 }
