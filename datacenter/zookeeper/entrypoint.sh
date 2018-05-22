18378b2e18a1a24ecfe32bab2b317e1e61a94d2f
humstarman <lueyoung7@gmail.com>
2018-05-22 13:26:03 +0800

    update: for no ip

--- datacenter/zookeeper/entrypoint.sh
+++ datacenter/zookeeper/entrypoint.sh
@@ -26,14 +26,13 @@ ALIAS=$(echo $THIS_NAME | awk -F '-' '{print $1}')
 ID=$(echo $THIS_NAME | awk -F '-' '{print $2}')
 MYID=$[${ID}+1]
 #ID=$(echo $THIS_NAME | awk -F '-' '{print $2}' | awk -F '.' '{print $1}')
-echo "$(date) - $0 - Nodes in this cluster: $N_NODES"
-echo "$(date) - $0 - IP: ${THIS_IP}"
-echo "$(date) - $0 - ID: ${ID}"
-echo "$(date) - $0 - MYID: ${MYID}"
-echo "$(date) - $0 - Alias: ${ALIAS}"
-echo "$(date) - $0 - svc discovery: $DSCV"
-
-service ssh start
+echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - $0 - Nodes in this cluster: $N_NODES"
+echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - $0 - IP: ${THIS_IP}"
+echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - $0 - ID: ${ID}"
+echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - $0 - MYID: ${MYID}"
+echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - $0 - Alias: ${ALIAS}"
+echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - $0 - svc discovery: $DSCV"
+echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - $0 - pod namespace: $POD_NAMESPACE"
 
 # set myid
 [ -e /mnt/$(hostname -s)/zookeeper ] || mkdir -p /mnt/$(hostname -s)/zookeeper
@@ -41,7 +40,7 @@ cd /mnt/$(hostname -s)/zookeeper && \
   touch myid && \
   echo "$MYID" > myid
 
-sed -i "s/var\/lib/mnt\/$(hostname -s)/g" /opt/zookeeper/conf/zoo.cfg
+sed -i s^"var/lib"^"mnt/$(hostname -s)"^g /opt/zookeeper/conf/zoo.cfg
 
 for i in $(seq -s ' ' 1 $N_NODES); do
   j=$[$i-1]
@@ -51,25 +50,8 @@ for i in $(seq -s ' ' 1 $N_NODES); do
     NAME=$THIS_NAME
   else
     NAME="${ALIAS}-$j"
-    IP=""
-    TRY=0
-    echo "server.$i=${NAME}:2888:3888" >> /opt/zookeeper/conf/zoo.cfg
-    while [ -z "$IP" ]; do
-      TRY=$[${TRY}+1]
-      if [ "$TRY" -gt "$TRIES" ]; then
-        echo "=== Cannot resolve the DNS entry for ${NAME}. Has the service been created yet, and is SkyDNS functional?"
-        echo "=== See http://kubernetes.io/v1.1/docs/admin/dns.html for more details on DNS integration."
-        echo "=== Sleeping ${WAIT}s before pod exit."
-        sleep $WAIT
-        exit 0
-      fi
-      IP=$(getent hosts $NAME.$DSCV | awk -F ' ' '{print $1}')
-      if [ -z "$IP" ]; then
-        sleep 1
-      fi
-    done
+    echo "server.$i=${NAME}.${DSCV}.${POD_NAMESPACE}:2888:3888" >> /opt/zookeeper/conf/zoo.cfg
   fi
-  echo -e "${IP}\t${NAME}" >> /etc/hosts
 done
 
 /opt/zookeeper/bin/zkServer.sh start-foreground
